0a5f70322586c537c4b91245b3b75ad0a0313323
Han <minatozaki1414@gmal.com>
2025-09-17 16:06:20 +0800

    Python

--- A_100DaysOfPython/Day1.py
+++ A_100DaysOfPython/Day1.py
@@ -1,9 +1,28 @@
-#Exercise 1
-#-- print ("Hello World!")
+print("Exercise 1")
+print ("Hello World!")
 
-#Exercise 2
-#-- print ("Day 1 - Python Print Function") 
-#-- print ("The function is declare like this:")
-#-- print ("print(what to print')")
+print("\n-----------------------------------------")
+
+print ("Exercise 2")
+print ("Day 1 - Python Print Function") 
+print ("The function is declare like this:")
+print ("print(what to print')")
+
+print("\n-----------------------------------------")
+
+print ("Exercise 3")
+print ("Hello World!\nHello World!\nHello World!")
+print ("Hello" + " " + "Hon")
+
+print("\n-----------------------------------------")
+
+#Fix the code below
+print ("Day 1 - String Manipulation")
+print ("String Concatenation is done with the '+' sign.")
+print ('e.g print("Hello " + "world")')
+print("(New lines can be created with a backlash and n.)")
+
+print("\n-----------------------------------------")
+
 
 
