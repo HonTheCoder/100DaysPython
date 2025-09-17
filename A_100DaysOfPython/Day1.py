e75613e2cf44d88df964f7ab83639f21a8b4c587
Han <minatozaki1414@gmal.com>
2025-09-17 15:49:43 +0800

    Python

--- A_100DaysOfPython/Day1.py
+++ A_100DaysOfPython/Day1.py
@@ -1,3 +1,9 @@
-#print hello world
-print ("Hello World!")
+#Exercise 1
+#-- print ("Hello World!")
+
+#Exercise 2
+#-- print ("Day 1 - Python Print Function") 
+#-- print ("The function is declare like this:")
+#-- print ("print(what to print')")
+
 
