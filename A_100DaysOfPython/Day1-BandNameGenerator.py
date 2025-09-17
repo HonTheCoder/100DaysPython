261ad676de2183641244abb7b5429114a2a2e1ae
Han <minatozaki1414@gmal.com>
2025-09-17 17:12:21 +0800

    Python

---
+++ A_100DaysOfPython/Day1-BandNameGenerator.py
@@ -0,0 +1,10 @@
+#BAND NAME GENERATOR 
+
+# Day 1 Project
+
+print("WELCOME TO BAND NAME GENERATOR!\n")
+
+city = input ("What's name of the city you grew up in?\n:")
+pet = input ("What is your pet's name?\n:")
+
+print("Your Band Name could be " + city + " " + pet + "." )
--- A_100DaysOfPython/Day1.py
+++ A_100DaysOfPython/Day1.py
@@ -1,30 +1,34 @@
-print("Exercise 1")
-print ("Hello World!")
+#-- print("Exercise 1")
+#-- print ("Hello World!")
 
 print("\n-----------------------------------------")
 
-print ("Exercise 2")
-print ("Day 1 - Python Print Function") 
-print ("The function is declare like this:")
-print ("print(what to print')")
+#-- print ("Exercise 2")
+#-- print ("Day 1 - Python Print Function") 
+#-- print ("The function is declare like this:")
+#-- print ("print(what to print')")
 
 print("\n-----------------------------------------")
 
-print ("Exercise 3")
-print ("Hello World!\nHello World!\nHello World!")
-print ("Hello" + " " + "Hon")
+#-- print ("Exercise 3")
+#-- print ("Hello World!\nHello World!\nHello World!")
+#-- print ("Hello" + " " + "Hon")
 
 print("\n-----------------------------------------")
 
 #Fix the code below
+
 #Missing double quotes (done)
-print ("Day 1 - String Manipulation")
+#-- print ("Day 1 - String Manipulation")
+
 #Outer double quotes changed to single (done)
-print ("String Concatenation is done with the '+' sign.")
+#-- print ("String Concatenation is done with the '+' sign.")
+
 #Indentation removed (done)
-print ('e.g print("Hello " + "world")')
+#-- print ('e.g print("Hello " + "world")')
+
 #Extra print function removed (done)
-print("(New lines can be created with a backlash and n.)")
+#-- print("(New lines can be created with a backlash and n.)")
 
 print("\n-----------------------------------------")
 
@@ -37,9 +41,34 @@ print("\n-----------------------------------------")
 
 print("\n-----------------------------------------")
 
-name = input("What is you name?")
+#Do it with variables
+#-- name = input("What is you name?")
+#-- print ("Your name has " + str(len(name)) + " letters")
 
-print ("Your name has " + str(len(name)) + " letters")
+#Do it with f strings
+#name = input("What is your name?") 
+#print (f"Your name has " + {(len(name)} + " letters")
 
-#OR -- print (f"Your name has " + {(len(name)} + " letters")
+#Do it in single line of code:
+#print ("Your name has " + str(len(input("What is your name?"))) + " letters")
 
+print("\n-----------------------------------------")
+
+#CODING EXERCISE :)
+#DON'T CHANGE THE CODE BELOW
+#-- a = input("A: ")
+#-- b = input("B: ")
+
+#CODE HERE
+
+#My Solution - What i did is use another variable named c and use it as an holder
+#              for a and i called out variable c on b so the print func will work.
+#-- c = a
+#-- a = b
+#-- b = c
+
+#CODE HERE
+
+#DON'T CHANGE THE CODE BELOW
+#-- print ("a = " + a)
+#-- print ("b = " + b)
