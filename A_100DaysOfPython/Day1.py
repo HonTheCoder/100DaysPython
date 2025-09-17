6543fa92e20921ab892882d8fcfc97ae560cbd76
Han <minatozaki1414@gmal.com>
2025-09-17 16:31:11 +0800

    Python

--- A_100DaysOfPython/Day1.py
+++ A_100DaysOfPython/Day1.py
@@ -17,12 +17,29 @@ print ("Hello" + " " + "Hon")
 print("\n-----------------------------------------")
 
 #Fix the code below
+#Missing double quotes (done)
 print ("Day 1 - String Manipulation")
+#Outer double quotes changed to single (done)
 print ("String Concatenation is done with the '+' sign.")
+#Indentation removed (done)
 print ('e.g print("Hello " + "world")')
+#Extra print function removed (done)
 print("(New lines can be created with a backlash and n.)")
 
 print("\n-----------------------------------------")
 
+#With Variables
+#-- name = input("What is your name?")
+#-- print ("Hello " + name)
 
+#Without Variables
+#-- print("Hello " + input("Name again?"))
+
+print("\n-----------------------------------------")
+
+name = input("What is you name?")
+
+print ("Your name has " + str(len(name)) + " letters")
+
+#OR -- print (f"Your name has " + {(len(name)} + " letters")
 
