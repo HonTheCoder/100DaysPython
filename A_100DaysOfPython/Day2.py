2efa4ff9da08a12ec7b8f5034e715fdbe5ef9ff6
Han <minatozaki1414@gmal.com>
2025-09-18 16:29:01 +0800

    Python

---
+++ A_100DaysOfPython/Day2.py
@@ -0,0 +1,83 @@
+#Copy paste area
+#-- 
+print("\n-----------------------------------------")
+#Copy paste area
+
+
+#Data Types 
+
+#String
+#-- print ("String")
+#Subscripting 
+#-- print ("Hello"[2])
+
+print("\n-----------------------------------------")
+
+#Integer
+#-- print (123 + 123)
+
+#-- 123_456_789 - doing this is like 123,456,789 para lang sa programmer
+#--               ang _ para makita na maayos 
+#Example
+#-- print (123_456 + 456_123)
+
+print("\n-----------------------------------------")
+
+#Float - decimals
+#-- 3.14159
+
+print("\n-----------------------------------------")
+
+#Boolean - 0 and 1 
+#-- True
+#-- False
+
+print("\n-----------------------------------------")
+
+#Type - type() = identifier
+#-- a = 1234
+#-- print (type(a))
+
+#We can convert something using float(), str(), int() etc.
+#Example 
+
+#-- print (str(100)) - should be int but i've used str() to convert it into string.
+
+print("\n-----------------------------------------")
+
+#Exercise - Adding 
+
+#Don't change the code below
+#-- two_digit_number = input("Type a two digit number: ")
+#Don't change the code below
+
+#My Solution code below is below this line
+#-- add = int(two_digit_number[0]) + int(two_digit_number[1])
+#-- print(add)
+
+#(Done)
+
+print("\n-----------------------------------------")
+
+#Operators (+ , - , * , / , **) ** = means to the power (exponent)
+
+print("\n-----------------------------------------")
+
+#Exercise - BMI Calculator
+
+#Don't change the code below
+#-- height = input ("enter your height in m: ")
+#-- weight = input ("enter your weight in kg: ")
+#Don't change the code below
+
+#My solution code below 
+#I've used round() to get just 2 decimal 
+
+#-- bmi =  float(weight) / float(height) ** 2
+
+#-- result = round(bmi , 2)
+
+#-- print (result)
+
+print("\n-----------------------------------------")
+
